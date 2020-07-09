48ce5d331082e4df4769a0096a2643f3ec67367c
wangyonghao <wangyonghao@rongcloud.cn>
2020-07-09 10:15:54 +0800

    feat: add project

---
+++ config.js
@@ -0,0 +1,9 @@
+const Config = {
+  appkey: 'e5t4ouvptkm2a', // appkey
+  anchor: [ // 主播 token
+    'dvbuCF3ewNdG8oKPJXsAsAojczDDPr8Tu8rBbZYr4GePAWcDOsjy2A==@5o9j.cn.rongnav.com;5o9j.cn.rongcfg.com'
+  ],
+  audience: [ // 观众 token
+    'dvbuCF3ewNcjlO8yvxIwEQojczDDPr8TZOPorFhuNrfq9GGtjrlb8g==@5o9j.cn.rongnav.com;5o9j.cn.rongcfg.com'
+  ]
+};
