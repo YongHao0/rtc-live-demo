383afdcb0b1166f5a447c99234c39dbe06a3cc4d
wangyonghao <wangyonghao@rongcloud.cn>
2020-08-18 17:43:43 +0800

    feat: update sdk

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const Config = {
-  appkey: 'e5t4ouvptkm2a', // appkey
+  appkey: 'sfci50a7songi', // appkey
   anchor: [ // 主播 token
-    'dvbuCF3ewNdG8oKPJXsAsAojczDDPr8Tu8rBbZYr4GePAWcDOsjy2A==@5o9j.cn.rongnav.com;5o9j.cn.rongcfg.com'
+    'FOxAx/Etj+y5zHpqJrvNGYpWJBRrbKy3qbHs3mzLKalZyCLdVxenvg==@1xkk.cn.rongnav.com;1xkk.cn.rongcfg.com'
   ],
   audience: [ // 观众 token
     'dvbuCF3ewNcjlO8yvxIwEQojczDDPr8TZOPorFhuNrfq9GGtjrlb8g==@5o9j.cn.rongnav.com;5o9j.cn.rongcfg.com'
